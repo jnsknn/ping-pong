f9b4ba56b4465655cd0c2776c3fe615c8516032b
Jonne Niskanen <jonne.niskanen@gmail.com>
2016-01-02 09:19:35 +0200

    Added a ball and a paddle.

--- PingPong/frmPingPong.Designer.cs
+++ PingPong/frmPingPong.Designer.cs
@@ -40,12 +40,21 @@
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.lblLivesLabel = new System.Windows.Forms.Label();
             this.lblScoreLabel = new System.Windows.Forms.Label();
+            this.lblLife = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.imgBall = new System.Windows.Forms.PictureBox();
+            this.imgPaddle = new System.Windows.Forms.PictureBox();
+            this.pnlPingPong.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPaddle)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlPingPong
             // 
             this.pnlPingPong.BackColor = System.Drawing.Color.Black;
+            this.pnlPingPong.Controls.Add(this.imgBall);
+            this.pnlPingPong.Controls.Add(this.imgPaddle);
             this.pnlPingPong.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.pnlPingPong.Location = new System.Drawing.Point(0, 61);
             this.pnlPingPong.Margin = new System.Windows.Forms.Padding(0);
@@ -78,25 +87,25 @@
             // newToolStripMenuItem
             // 
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
             this.newToolStripMenuItem.Text = "New";
             // 
             // pauseToolStripMenuItem
             // 
             this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
-            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
             this.pauseToolStripMenuItem.Text = "Pause";
             // 
             // highScoresToolStripMenuItem
             // 
             this.highScoresToolStripMenuItem.Name = "highScoresToolStripMenuItem";
-            this.highScoresToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.highScoresToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
             this.highScoresToolStripMenuItem.Text = "High scores";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -112,34 +121,72 @@
             // manualToolStripMenuItem
             // 
             this.manualToolStripMenuItem.Name = "manualToolStripMenuItem";
-            this.manualToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.manualToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.manualToolStripMenuItem.Text = "Manual";
             // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
-            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.aboutToolStripMenuItem.Text = "About PingPong";
             // 
             // lblLivesLabel
             // 
             this.lblLivesLabel.AutoSize = true;
-            this.lblLivesLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLivesLabel.Location = new System.Drawing.Point(12, 24);
+            this.lblLivesLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLivesLabel.Location = new System.Drawing.Point(12, 40);
             this.lblLivesLabel.Name = "lblLivesLabel";
-            this.lblLivesLabel.Size = new System.Drawing.Size(65, 24);
+            this.lblLivesLabel.Size = new System.Drawing.Size(50, 19);
             this.lblLivesLabel.TabIndex = 2;
             this.lblLivesLabel.Text = "Lives";
             // 
             // lblScoreLabel
             // 
             this.lblScoreLabel.AutoSize = true;
-            this.lblScoreLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblScoreLabel.Location = new System.Drawing.Point(519, 24);
+            this.lblScoreLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreLabel.Location = new System.Drawing.Point(95, 40);
             this.lblScoreLabel.Name = "lblScoreLabel";
-            this.lblScoreLabel.Size = new System.Drawing.Size(69, 24);
+            this.lblScoreLabel.Size = new System.Drawing.Size(54, 19);
             this.lblScoreLabel.TabIndex = 3;
             this.lblScoreLabel.Text = "Score";
+            // 
+            // lblLife
+            // 
+            this.lblLife.AutoSize = true;
+            this.lblLife.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLife.Location = new System.Drawing.Point(68, 40);
+            this.lblLife.Name = "lblLife";
+            this.lblLife.Size = new System.Drawing.Size(17, 18);
+            this.lblLife.TabIndex = 4;
+            this.lblLife.Text = "3";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(155, 40);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(17, 18);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "0";
+            // 
+            // imgBall
+            // 
+            this.imgBall.Image = global::PingPong.Properties.Resources.ball;
+            this.imgBall.Location = new System.Drawing.Point(1, 1);
+            this.imgBall.Name = "imgBall";
+            this.imgBall.Size = new System.Drawing.Size(10, 10);
+            this.imgBall.TabIndex = 1;
+            this.imgBall.TabStop = false;
+            // 
+            // imgPaddle
+            // 
+            this.imgPaddle.Image = global::PingPong.Properties.Resources.paddle;
+            this.imgPaddle.Location = new System.Drawing.Point(275, 385);
+            this.imgPaddle.Name = "imgPaddle";
+            this.imgPaddle.Size = new System.Drawing.Size(50, 10);
+            this.imgPaddle.TabIndex = 0;
+            this.imgPaddle.TabStop = false;
             // 
             // frmPingPong
             // 
@@ -147,6 +194,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(600, 461);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblLife);
             this.Controls.Add(this.lblScoreLabel);
             this.Controls.Add(this.lblLivesLabel);
             this.Controls.Add(this.pnlPingPong);
@@ -155,8 +204,11 @@
             this.MaximizeBox = false;
             this.Name = "frmPingPong";
             this.Text = "PingPong master";
+            this.pnlPingPong.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPaddle)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -176,6 +228,10 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.Label lblLivesLabel;
         private System.Windows.Forms.Label lblScoreLabel;
+        private System.Windows.Forms.Label lblLife;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.PictureBox imgPaddle;
+        private System.Windows.Forms.PictureBox imgBall;
     }
 }
 
